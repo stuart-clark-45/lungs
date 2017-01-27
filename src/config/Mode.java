918ed6d1edf6761f4184fdb35ac157727d4067e9
Stuart <stuart.clark_45@live.co.uk>
2017-01-27 13:06:51 +0000

    Mode: created

---
+++ src/config/Mode.java
@@ -0,0 +1,16 @@
+package config;
+
+/**
+ * The key and values for the system mode configuration variable
+ *
+ * @author Stuart Clark
+ */
+public class Mode {
+
+  public static final String KEY = "mode";
+
+  public enum VALUE {
+    PROD, TEST
+  }
+
+}
