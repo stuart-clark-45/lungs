77da2b8910fd023ae0327e93ee2c61e1cd22f992
Stuart <stuart.clark_45@live.co.uk>
2017-01-27 12:38:44 +0000

    ConfigHelperTest: added testGetString(..) and clean up

--- test/util/ConfigHelperTest.java
+++ test/util/ConfigHelperTest.java
@@ -1,9 +1,11 @@
 package util;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.apache.commons.configuration.ConfigurationMap;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,6 +14,9 @@ import org.junit.Test;
  */
 public class ConfigHelperTest {
 
+  private static final String TESTING123 = "testing123";
+  private static final String TESTING456 = "testing456";
+
   private ConfigurationMap props;
 
   @Before
@@ -19,25 +24,32 @@ public class ConfigHelperTest {
     props = ConfigHelper.getProps();
   }
 
+  @After
+  public void tearDown() throws Exception {
+    props.remove(TESTING123);
+    props.remove(TESTING456);
+  }
+
   @Test
   public void testGetBoolean() throws Exception {
-    String testing123 = "testing123";
-    String testing456 = "testing456";
+    props.put(TESTING123, "true");
+    props.put(TESTING456, "false");
 
-    props.put(testing123, "true");
-    props.put(testing456, "false");
-
-    assertTrue(ConfigHelper.getBoolean(testing123));
-    assertFalse(ConfigHelper.getBoolean(testing456));
+    assertTrue(ConfigHelper.getBoolean(TESTING123));
+    assertFalse(ConfigHelper.getBoolean(TESTING456));
   }
 
   @Test(expected = IllegalStateException.class)
-  public void testBadValue() throws Exception {
-    String testing123 = "testing123";
+  public void testBadGetBoolean() throws Exception {
+    props.put(TESTING123, "not true or false");
+    ConfigHelper.getBoolean(TESTING123);
+  }
 
-    props.put(testing123, "not true or false");
-
-    ConfigHelper.getBoolean(testing123);
+  @Test
+  public void testGetString() throws Exception {
+    String value = "value";
+    props.put(TESTING123, value);
+    assertEquals(value, ConfigHelper.getString(TESTING123));
   }
 
   @Test(expected = IllegalStateException.class)
