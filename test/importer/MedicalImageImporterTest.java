9057268dbf553a07498a335d1d567d2decc5ac1b
Stuart <stuart.clark_45@live.co.uk>
2017-01-29 23:20:07 +0000

    MedicalImporterTest: testing correct number imported

--- test/importer/MedicalImageImporterTest.java
+++ test/importer/MedicalImageImporterTest.java
@@ -1,8 +1,12 @@
 package importer;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import model.MedicalImage;
+import util.MongoHelper;
 import util.PreTest;
 
 /**
@@ -13,7 +17,10 @@ public class MedicalImageImporterTest {
 
   @Test
   public void test() throws Exception {
-    new MedicalImageImporter().call();
+    new MedicalImageImporter().run();
+
+    // Should only be one as only one of the images is a CT scan
+    assertEquals(1, MongoHelper.getDataStore().createQuery(MedicalImage.class).count());
   }
 
 }
