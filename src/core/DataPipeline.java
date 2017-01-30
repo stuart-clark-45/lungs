366a76bc5a484ae9235760879aea0a864e1300d7
Stuart <stuart.clark_45@live.co.uk>
2017-01-30 00:19:57 +0000

    DataPipeline: logging

--- src/core/DataPipeline.java
+++ src/core/DataPipeline.java
@@ -2,6 +2,8 @@ package core;
 
 import generator.CTStackGenerator;
 import importer.CTSliceImporter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Used to import and aggregate all of the data required for the system to be run.
@@ -10,9 +12,15 @@ import importer.CTSliceImporter;
  */
 public class DataPipeline {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(DataPipeline.class);
+
   public static void main(String[] args) {
+    LOGGER.info("Running DataPineLine");
+
     new CTSliceImporter().run();
     new CTStackGenerator().run();
+
+    LOGGER.info("DataPineLine complete.");
   }
 
 }
