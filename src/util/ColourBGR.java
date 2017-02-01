fa5c81eba8fabcb6d8083528824031fc6939835c
Stuart <stuart.clark_45@live.co.uk>
2017-02-01 19:06:19 +0000

    ColourBGR: created

---
+++ src/util/ColourBGR.java
@@ -0,0 +1,18 @@
+package util;
+
+/**
+ * Used for obtaining commonly used colours in BGR (blue, green, reg colour space).
+ *
+ * @author Stuart Clark
+ */
+public class ColourBGR {
+
+  public static final double[] RED = new double[] {0, 0, 255};
+  public static final double[] GREEN = new double[] {0, 255, 0};
+  public static final double[] BLUE = new double[] {255, 0, 0};
+
+  private ColourBGR() {
+    // Hide constructor
+  }
+
+}
