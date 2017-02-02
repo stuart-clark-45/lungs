4cb6e6b50e1f72a8dcc9bf6cefd034d6fb558166
Stuart <stuart.clark_45@live.co.uk>
2017-02-02 16:15:50 +0000

    Annotation: created

---
+++ src/config/Annotation.java
@@ -0,0 +1,18 @@
+package config;
+
+/**
+ * The keys annotation configuration variables. No values as they are boolean variables.
+ *
+ * @author Stuart Clark
+ */
+public class Annotation {
+
+  public static final String BIG_NODULE = "annotation.bignodule";
+  public static final String SMALL_NODULE = "annotation.smallnodule";
+  public static final String NON_NODULE = "annotation.nonnodule";
+
+  private Annotation() {
+    // Hide the constructor
+  }
+
+}
