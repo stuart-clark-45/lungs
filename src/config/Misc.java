076fadb89333cb0565edb7fe76e73d13184649a3
Stuart <stuart.clark_45@live.co.uk>
2017-02-06 19:21:33 +0000

    Misc: created

---
+++ src/config/Misc.java
@@ -0,0 +1,12 @@
+package config;
+
+/**
+ * The keys for miscellaneous configuration variables.
+ *
+ * @author Stuart Clark
+ */
+public class Misc {
+
+  public static final String MATCH_THRESHOLD = "matchthreshold";
+
+}
