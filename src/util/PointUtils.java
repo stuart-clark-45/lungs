b64fe1f4250bf5f5fb12cac047727850d27c6854
Stuart <stuart.clark_45@live.co.uk>
2017-02-06 17:16:09 +0000

    PointUtils: dedupe(..) and perim2Region(..)

--- src/util/PointUtils.java
+++ src/util/PointUtils.java
@@ -3,20 +3,98 @@ package util;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+import vision.ROIExtractor;
 
 /**
- * Suite of utility methods associated with {@link Point}s
+ * Suite of utility methods associated with {@link Point}s.
  *
  * @author Stuart Clark
  */
 public class PointUtils {
 
+  private static final int FORE_GROUND = 255;
+
   private PointUtils() {
     // Hide the constructor
+  }
+
+  /**
+   * @param perimeter a list of {@link Point}s, must be an unbroken parameter.
+   * @param inclusive true if the points of the parameter should be included in the returned region,
+   *        false otherwise.
+   * @return a list of {@link Point}s that contains every {@link Point} in the region that
+   *         corresponds to {@code perimeter}. Order is not guaranteed to be maintained.
+   */
+  public static List<Point> perim2Region(List<Point> perimeter, boolean inclusive)
+      throws LungsException {
+    // Remove duplicates if there are any
+    perimeter = dedupe(perimeter);
+
+    // Get first point
+    Point first = perimeter.get(0);
+
+    // Find max x and y values and correctly set the first point
+    double xMax = first.x;
+    double yMax = first.y;
+    for (int i = 1; i < perimeter.size(); i++) {
+      Point point = perimeter.get(i);
+
+      if (point.x > xMax) {
+        xMax = point.x;
+      }
+
+      if (point.y > yMax) {
+        yMax = point.y;
+      }
+
+    }
+
+    // Create a mat with the filled region
+    Mat filled = Mat.zeros((int) yMax + 1, (int) xMax + 1, CvType.CV_8UC1);
+    Point[] pointArray = perimeter.toArray(new Point[perimeter.size()]);
+    Imgproc.fillPoly(filled, Collections.singletonList(new MatOfPoint(pointArray)), new Scalar(
+        FORE_GROUND));
+
+    // Extract the region points
+    ROIExtractor extractor = new ROIExtractor(FORE_GROUND);
+    List<Point> region = extractor.extractOne(filled, first).getPoints();
+
+    // Remove the perimeter if it was not inclusive
+    if (!inclusive) {
+      region.removeAll(perimeter);
+    }
+
+    return region;
+  }
+
+  /**
+   * @param points
+   * @return {@code points} with any duplicates removed and order maintained.
+   */
+  public static List<Point> dedupe(List<Point> points) {
+    List<Point> noDupes = new ArrayList<>(points.size());
+    Set<Point> seen = new HashSet<>();
+
+    for (Point next : points) {
+      if (!seen.contains(next)) {
+        noDupes.add(next);
+        seen.add(next);
+      }
+    }
+
+    return noDupes;
   }
 
   /**
--- test/util/PointUtilsTest.java
+++ test/util/PointUtilsTest.java
@@ -1,40 +1,86 @@
 package util;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.opencv.core.Point;
-
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opencv.core.Point;
 
 /**
  * @author Stuart Clark
  */
+@RunWith(Testing.class)
 public class PointUtilsTest {
 
-  private List<Point> unsorted = new ArrayList<>();
-  private List<Point> sorted = new ArrayList<>();
+  @Test
+  public void testPerim2Region() throws Exception {
+    List<Point> perimeter = new ArrayList<>();
+    perimeter.add(new Point(2, 6));
+    perimeter.add(new Point(3, 5));
+    perimeter.add(new Point(1, 5));
+    perimeter.add(new Point(4, 4));
+    perimeter.add(new Point(0, 4));
+    perimeter.add(new Point(3, 3));
+    perimeter.add(new Point(1, 3));
+    perimeter.add(new Point(2, 2));
 
-  @Before
-  public void setUp() throws Exception {
-    unsorted = new ArrayList<>();
+    // Test exclusive
+    Set<Point> exclusive = new HashSet<>();
+    exclusive.add(new Point(2, 3));
+    exclusive.add(new Point(1, 4));
+    exclusive.add(new Point(2, 4));
+    exclusive.add(new Point(3, 4));
+    exclusive.add(new Point(2, 5));
+    assertEquals(exclusive, new HashSet<>(PointUtils.perim2Region(perimeter, false)));
+
+    // Test inclusive
+    Set<Point> inclusive = new HashSet<>(perimeter);
+    inclusive.addAll(exclusive);
+    assertEquals(inclusive, new HashSet<>(PointUtils.perim2Region(perimeter, true)));
+  }
+
+  @Test
+  public void testXySort() throws Exception {
+    List<Point> unsorted = new ArrayList<>();
     unsorted.add(new Point(2, 2));
     unsorted.add(new Point(1, 2));
     unsorted.add(new Point(2, 1));
     unsorted.add(new Point(1, 1));
 
-    sorted = new ArrayList<>();
+    List<Point> sorted = new ArrayList<>();
     sorted.add(new Point(1, 1));
     sorted.add(new Point(2, 1));
     sorted.add(new Point(1, 2));
     sorted.add(new Point(2, 2));
+
+    assertEquals(sorted, PointUtils.xySort(unsorted));
   }
 
   @Test
-  public void testXySort() throws Exception {
-    assertEquals(sorted, PointUtils.xySort(unsorted));
+  public void testDeDupe() throws Exception {
+    List<Point> points = new ArrayList<>();
+    points.add(new Point(1, 1));
+    points.add(new Point(1, 1));
+    points.add(new Point(1, 1));
+    points.add(new Point(2, 2));
+    points.add(new Point(2, 2));
+    points.add(new Point(2, 2));
+    points.add(new Point(2, 2));
+    points.add(new Point(3, 3));
+    points.add(new Point(3, 3));
+    points.add(new Point(3, 3));
+
+    List<Point> expected = new ArrayList<>();
+    expected.add(new Point(1, 1));
+    expected.add(new Point(2, 2));
+    expected.add(new Point(3, 3));
+
+    assertEquals(expected, PointUtils.dedupe(points));
   }
 
 }
