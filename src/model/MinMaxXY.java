c2fc2d141eee3161f1c71c19e951f541c535b0f6
Stuart <stuart.clark_45@live.co.uk>
2017-02-09 17:53:39 +0000

    MinMaxXY: public fields

--- src/model/MinMaxXY.java
+++ src/model/MinMaxXY.java
@@ -7,41 +7,9 @@ package model;
  */
 public class MinMaxXY<T> {
 
-  private T minX;
-  private T maxX;
-  private T minY;
-  private T maxY;
-
-  public T getMinX() {
-    return minX;
-  }
-
-  public void setMinX(T minX) {
-    this.minX = minX;
-  }
-
-  public T getMaxX() {
-    return maxX;
-  }
-
-  public void setMaxX(T maxX) {
-    this.maxX = maxX;
-  }
-
-  public T getMinY() {
-    return minY;
-  }
-
-  public void setMinY(T minY) {
-    this.minY = minY;
-  }
-
-  public T getMaxY() {
-    return maxY;
-  }
-
-  public void setMaxY(T maxY) {
-    this.maxY = maxY;
-  }
+  public T minX;
+  public T maxX;
+  public T minY;
+  public T maxY;
 
 }
