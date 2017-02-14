449340de2aa7d20a6f891c5640106a48b9380df6
Stuart <stuart.clark_45@live.co.uk>
2017-02-14 20:04:55 +0000

    Area: created

---
+++ src/ml/feature/Area.java
@@ -0,0 +1,20 @@
+package ml.feature;
+
+import org.opencv.core.Mat;
+
+import model.ROI;
+
+/**
+ * Computes the area of the {@link ROI} and updates {@link ROI#area}.
+ * 
+ * @author Stuart Clark
+ */
+public class Area implements Feature {
+
+  @Override
+  public void compute(ROI roi, Mat mat) {
+    // TODO need to measure using info from meta data not just count number of pixels
+    roi.setArea(roi.getRegion().size());
+  }
+
+}
