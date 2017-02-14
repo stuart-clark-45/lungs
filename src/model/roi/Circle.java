dc6464d4b60059fd63a59e0bba8207f8ba042303
Stuart <stuart.clark_45@live.co.uk>
2017-02-14 21:58:31 +0000

    Circle: constructor

--- src/model/roi/Circle.java
+++ src/model/roi/Circle.java
@@ -12,6 +12,15 @@ public class Circle {
   private Point center;
   private double radius;
 
+  private Circle() {
+    // For morphia
+  }
+
+  public Circle(Point center, double radius) {
+    this.center = center;
+    this.radius = radius;
+  }
+
   public Point getCenter() {
     return center;
   }
