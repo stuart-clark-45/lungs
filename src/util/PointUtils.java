ff8c8ba1f9f06db32ffbafaf6b0ec8fe738cb2f7
Stuart <stuart.clark_45@live.co.uk>
2017-02-14 11:23:29 +0000

    PointUtils: external contour only

--- src/util/PointUtils.java
+++ src/util/PointUtils.java
@@ -74,10 +74,11 @@ public class PointUtils {
     MinMaxXY<Double> mmXY = xyMaxMin(regionPoints);
     Mat region = points2MinMat(regionPoints, mmXY);
     List<MatOfPoint> contours = new ArrayList<>();
-    Imgproc.findContours(region, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_NONE);
-    if(contours.size() != 1) {
-      throw new LungsException("More than one contour has been found");
-    }
+
+    // Find external contour
+    Imgproc.findContours(region, contours, new Mat(), Imgproc.RETR_EXTERNAL,
+        Imgproc.CHAIN_APPROX_NONE);
+
     // There should only every be one contour
     List<Point> perimeter = contours.get(0).toList();
     // add the min vals back to each x and y
