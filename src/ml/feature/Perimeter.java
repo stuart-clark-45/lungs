e9161844ce00cd615887635433ef950eab623a2f
Stuart <stuart.clark_45@live.co.uk>
2017-02-14 20:05:04 +0000

    Perimieter: created

---
+++ src/ml/feature/Perimeter.java
@@ -0,0 +1,21 @@
+package ml.feature;
+
+import org.opencv.core.Mat;
+
+import model.ROI;
+import util.LungsException;
+
+/**
+ * Computes the perimeter length for the {@link ROI} and updates {@link ROI#perimLength}.
+ *
+ * @author Stuart Clark
+ */
+public class Perimeter implements Feature {
+
+  @Override
+  public void compute(ROI roi, Mat mat) throws LungsException {
+    // TODO need to measure using info from meta data not just count number of pixels
+    roi.setPerimLength(roi.getPerimeter().size());
+  }
+
+}
