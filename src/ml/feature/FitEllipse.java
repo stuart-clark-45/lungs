50e4fc8f66753e64ad0c037d7d2eede53db89d90
Stuart <stuart.clark_45@live.co.uk>
2017-02-15 15:04:51 +0000

    FitElilipse: checking there is enough points

--- src/ml/feature/FitEllipse.java
+++ src/ml/feature/FitEllipse.java
@@ -2,10 +2,13 @@ package ml.feature;
 
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
 import org.opencv.imgproc.Imgproc;
 
 import model.roi.ROI;
 import util.LungsException;
+
+import java.util.List;
 
 /**
  * Fits an ellipse to the {@link ROI} and updates {@link ROI#fitEllipse}.
@@ -16,8 +19,15 @@ public class FitEllipse implements Feature {
 
   @Override
   public void compute(ROI roi, Mat mat) throws LungsException {
+    List<Point> contour = roi.getContour();
+
+    // Check if contour is large enough
+    if(contour.size() < 5){
+      return;
+    }
+
     MatOfPoint2f matOfPoint = new MatOfPoint2f();
-    matOfPoint.fromList(roi.getContour());
+    matOfPoint.fromList(contour);
     roi.setFitEllipse(Imgproc.fitEllipse(matOfPoint));
   }
 
