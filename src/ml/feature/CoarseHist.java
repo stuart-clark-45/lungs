ee04ec3d99720d7636be7b12d68ac8654677a1c4
Stuart <stuart.clark_45@live.co.uk>
2017-02-15 12:41:08 +0000

    CoarsHist: created

---
+++ src/ml/feature/CoarseHist.java
@@ -0,0 +1,24 @@
+package ml.feature;
+
+import org.opencv.core.Mat;
+
+import model.roi.ROI;
+import util.LungsException;
+
+/**
+ * Creates a histogram with few bins for the {@link ROI} and updates {@link ROI#coarseHist}.
+ *
+ * @author Stuart Clark
+ */
+public class CoarseHist extends IntensityHist {
+
+  public CoarseHist() {
+    super(20);
+  }
+
+  @Override
+  public void compute(ROI roi, Mat mat) throws LungsException {
+    roi.setCoarseHist(createHist(roi, mat));
+  }
+
+}
