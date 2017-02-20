d13d1be8abd114d086efb4312565d2151e205cbb
Stuart <stuart.clark_45@live.co.uk>
2017-02-20 09:40:24 +0000

    LimitedIterator: created

---
+++ test/util/LimitedIteratorTest.java
@@ -0,0 +1,31 @@
+package util;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+/**
+ * @author Stuart Clark
+ */
+public class LimitedIteratorTest {
+
+  @Test
+  public void test() throws Exception {
+    List<String> input = Arrays.asList("1", "2", "3");
+    LimitedIterator<String> iterator = new LimitedIterator<>(input.iterator(), 2);
+
+    List<String> output = new ArrayList<>();
+    while (iterator.hasNext()){
+      output.add(iterator.next());
+    }
+    assertEquals(Arrays.asList("1", "2"), output);
+
+    assertNull(iterator.next());
+  }
+
+}
