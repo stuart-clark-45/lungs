558ca68146f535e4f65004dee6781fe1cf5ed219
Stuart <stuart.clark_45@live.co.uk>
2017-02-21 18:23:30 +0000

    SliceSegementer: returning edges

--- src/vision/SliceSegmenter.java
+++ src/vision/SliceSegmenter.java
@@ -1,6 +1,15 @@
 package vision;
 
+import static org.opencv.imgproc.Imgproc.COLORMAP_HSV;
+import static org.opencv.imgproc.Imgproc.THRESH_BINARY;
+import static org.opencv.imgproc.Imgproc.applyColorMap;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import util.MatUtils;
 
 /**
  * Used segment CT slices.
@@ -25,9 +34,48 @@ public class SliceSegmenter {
   }
 
   public Mat segment(Mat original) {
+    Mat foregroundMat = MatUtils.similarMat(original);
+    Imgproc.threshold(original, foregroundMat, sureFG, foreground, THRESH_BINARY);
 
-    return original;
+    Mat backgroundMat = MatUtils.similarMat(original);
+    Imgproc.threshold(original, backgroundMat, sureBG, foreground, THRESH_BINARY);
 
+    Mat unknownMat = MatUtils.similarMat(original);
+    Core.subtract(backgroundMat, foregroundMat, unknownMat);
+
+    // Temp label with become the wrong CvType when connected Components is called
+    Mat labels = MatUtils.similarMat(original);
+    Imgproc.connectedComponents(foregroundMat, labels);
+
+    // Add one to each of the labels so that we can mark the unknown region with 0's
+    for (int row = 0; row < labels.rows(); row++) {
+      for (int col = 0; col < labels.cols(); col++) {
+        labels.put(row, col, labels.get(row, col)[0] + 1);
+      }
+    }
+
+    // Mark the unknown region with 0's
+    for (int row = 0; row < labels.rows(); row++) {
+      for (int col = 0; col < labels.cols(); col++) {
+        if (unknownMat.get(row, col)[0] == foreground) {
+          labels.put(row, col, 0);
+        }
+      }
+    }
+
+    Mat bgr = MatUtils.grey2BGR(original);
+    Imgproc.watershed(bgr, labels);
+
+    Mat thresholded = MatUtils.similarMat(original);
+    for (int row = 0; row < labels.rows(); row++) {
+      for (int col = 0; col < labels.cols(); col++) {
+        if (labels.get(row, col)[0] == -1) {
+          thresholded.put(row, col, foreground);
+        }
+      }
+    }
+
+    return thresholded;
   }
 
 }
