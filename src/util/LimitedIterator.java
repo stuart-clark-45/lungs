f299c3ee0334f031a7ab8bdddfaa61ab184a08d9
Stuart <stuart.clark_45@live.co.uk>
2017-02-27 18:08:07 +0000

    LimitedIterator: resurected

---
+++ src/util/LimitedIterator.java
@@ -0,0 +1,39 @@
+package util;
+
+import java.util.Iterator;
+
+/**
+ * Used to place a limit on the number of elements that can be obtained from an {@link Iterator}.
+ *
+ * @author Stuart Clark
+ */
+public class LimitedIterator<T> implements Iterator<T> {
+
+  private final Iterator<T> iterator;
+  private final int limit;
+  private int counter;
+
+  /**
+   * @param iterator the {@link Iterator} to limit.
+   * @param limit the number of times it should be possible to call {@link LimitedIterator#next()}.
+   */
+  public LimitedIterator(Iterator<T> iterator, int limit) {
+    this.iterator = iterator;
+    this.limit = limit;
+    this.counter = 0;
+  }
+
+  @Override
+  public boolean hasNext() {
+    return iterator.hasNext() && counter < limit;
+  }
+
+  @Override
+  public T next() {
+    if (counter++ >= limit) {
+      return null;
+    }
+    return iterator.next();
+  }
+
+}
