d731494d49b838578fbfe942ae86378fb7280ca8
Stuart <stuart.clark_45@live.co.uk>
2017-03-09 02:45:39 +0000

    ColourBGR: 16bit colour

--- src/util/ColourBGR.java
+++ src/util/ColourBGR.java
@@ -1,5 +1,7 @@
 package util;
 
+import static java.lang.Short.MAX_VALUE;
+
 /**
  * Used for obtaining commonly used colours in BGR (blue, green, reg colour space).
  *
@@ -7,11 +9,11 @@ package util;
  */
 public class ColourBGR {
 
-  public static final double[] WHITE = new double[] {255, 255, 255};
-  public static final double[] RED = new double[] {0, 0, 255};
-  public static final double[] GREEN = new double[] {0, 255, 0};
-  public static final double[] BLUE = new double[] {255, 0, 0};
-  public static final double[] ORANGE = new double[] {0, 165, 255};
+  public static final double[] WHITE = new double[] {MAX_VALUE, MAX_VALUE, MAX_VALUE};
+  public static final double[] RED = new double[] {0, 0, MAX_VALUE};
+  public static final double[] GREEN = new double[] {0, MAX_VALUE, 0};
+  public static final double[] BLUE = new double[] {MAX_VALUE, 0, 0};
+  public static final double[] ORANGE = new double[] {0, MAX_VALUE / 2, MAX_VALUE};
 
   private ColourBGR() {
     // Hide constructor
