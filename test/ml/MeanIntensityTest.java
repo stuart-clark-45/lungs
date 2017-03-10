3bc4937e75359a4a510083b60c42beab7730f996
Stuart <stuart.clark_45@live.co.uk>
2017-03-10 16:02:27 +0000

    MeanIntensityTest: points had x and y mixed up

--- test/ml/MeanIntensityTest.java
+++ test/ml/MeanIntensityTest.java
@@ -22,13 +22,13 @@ public class MeanIntensityTest {
   public void test() throws Exception {
     ROI roi = new ROI();
     roi.addPoint(new Point(0, 0));
-    roi.addPoint(new Point(0, 1));
-    roi.addPoint(new Point(0, 2));
-    roi.addPoint(new Point(0, 3));
     roi.addPoint(new Point(1, 0));
+    roi.addPoint(new Point(2, 0));
+    roi.addPoint(new Point(3, 0));
+    roi.addPoint(new Point(0, 1));
     roi.addPoint(new Point(1, 1));
-    roi.addPoint(new Point(1, 2));
-    roi.addPoint(new Point(1, 3));
+    roi.addPoint(new Point(2, 1));
+    roi.addPoint(new Point(3, 1));
 
     Mat mat = Imgcodecs.imread("./testres/quarter-white.bmp", Imgcodecs.IMREAD_GRAYSCALE);
 
