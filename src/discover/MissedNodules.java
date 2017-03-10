73f246c11e230c45672a0c55aaca9883968f70c3
Stuart <stuart.clark_45@live.co.uk>
2017-03-10 16:06:18 +0000

    MissedNodules: serperated into dark and light

--- src/discover/MissedNodules.java
+++ src/discover/MissedNodules.java
@@ -1,8 +1,10 @@
 package discover;
 
+import static discover.HistogramWriter.BINS;
 import static model.GroundTruth.Type.BIG_NODULE;
 import static util.MatUtils.getSliceMat;
 import static util.MatUtils.grey2BGR;
+import static util.MatUtils.put;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -10,6 +12,7 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.query.Query;
@@ -29,6 +32,7 @@ import util.ConfigHelper;
 import util.DataFilter;
 import util.FutureMonitor;
 import util.LungsException;
+import util.MatUtils;
 import util.MongoHelper;
 import util.MultiMap;
 
@@ -38,14 +42,21 @@ import util.MultiMap;
  *
  * @author Stuart Clark
  */
-public class MissedNodules extends HistogramWriter {
+public class MissedNodules {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(MissedNodules.class);
   private static final String ARG_ERROR =
       "Please provide a single argument either --images or --noimages";
   private static final String IMAGE_SOP_UID = "imageSopUID";
   private static final String IMAGE_DIR = ConfigHelper.getString(Misc.MISSED_NODULES);
+  private static final String DARK_DIR = IMAGE_DIR + "/dark";
+  private static final String LIGHT_DIR = IMAGE_DIR + "/light";
   private static final int LOG_INTERVAL = 200;
+
+  /**
+   * The threshold used to distinguish between dark and light nodules.
+   */
+  private static final double DARK_LIGHT_THRESH = 65;
 
   private final ExecutorService es;
 
@@ -57,46 +68,59 @@ public class MissedNodules extends HistogramWriter {
   private final Datastore ds;
   private final DataFilter filter;
 
+  /**
+   * Used to create a csv containing histogram data for nodules that have a very dark ROIs.
+   */
+  private final HistogramWriter darkWriter;
+
+  /**
+   * Used to create a csv containing histogram data for nodules that have a very light ROIs (these
+   * are likely to be juxtapleural nodules that are missed).
+   */
+  private final HistogramWriter lightWriter;
+
+  /**
+   * Used to count the number of nodules that have 0 intensity for every pixel.
+   */
+  private AtomicInteger allBlackNodules;
+
   public MissedNodules(ExecutorService es, boolean images) throws LungsException {
     super();
     this.es = es;
     this.images = images;
     this.ds = MongoHelper.getDataStore();
     this.filter = DataFilter.get();
+
+    this.darkWriter = new HistogramWriter() {
+      @Override
+      protected String fileName() {
+        return "dark-nodules.csv";
+      }
+    };
+
+
+    this.lightWriter = new HistogramWriter() {
+      @Override
+      protected String fileName() {
+        return "light-nodules.csv";
+      }
+    };
+
   }
 
-  @Override
-  protected String fileName() {
-    return "missed-nodules-hists.csv";
-  }
-
-  @Override
-  public void writeToFile() throws LungsException {
+  public void run() throws LungsException {
     LOGGER.info("MissedNodules running...");
 
-    // Get all the nodules that have been missed
-    Query<GroundTruth> query =
-        filter.singleReading(filter.all(ds.createQuery(GroundTruth.class).field("type")
-            .equal(BIG_NODULE).field("matchedToRoi").equal(false)));
-    long numMissed = query.count();
-    LOGGER.info(numMissed + " NODULES have been missed");
+    // Create the imageSopUID to GroundTruth multimap
+    MultiMap<String, GroundTruth> uidToGt = buildUidToGt();
 
-    // Collect the nodules by imageSopUID
-    MultiMap<String, GroundTruth> uidToGt = new MultiMap<>();
-    query.cloneQuery().forEach(gt -> uidToGt.putOne(gt.getImageSopUID(), gt));
-    LOGGER.info(uidToGt.size() + " slices have missing nodules");
+    // Reset the image directories
+    if (images) {
+      resetDirs();
+    }
 
-    // Create images if required
-    analise(uidToGt);
-
-    LOGGER.info("MissedNodules finished running");
-  }
-
-  private void analise(MultiMap<String, GroundTruth> uidToGt) {
-    // Clear the image directory
-    File dir = new File(IMAGE_DIR);
-    dir.delete();
-    dir.mkdir();
+    // Reset the black nodule counter
+    allBlackNodules = new AtomicInteger(0);
 
     // Create a future for each of the slices that needs annotating
     int numSlice = uidToGt.size();
@@ -106,45 +130,49 @@ public class MissedNodules extends HistogramWriter {
 
       // Submit runnable for slice
       futures.add(es.submit(() -> {
-        try {
-          // Get Mats for the slice
+        // Get Mats for the slice
           CTSlice slice = ds.createQuery(CTSlice.class).field(IMAGE_SOP_UID).equal(sopUID).get();
           Mat mat = getSliceMat(slice);
-          Mat bgr = null;
-          if (images) {
-            bgr = grey2BGR(mat);
-          }
 
           // Annotate GroundTruths on Mat
           for (GroundTruth gt : uidToGt.get(sopUID)) {
 
+            // Decide if the nodule is a dark or light nodule
+            List<Point> region = gt.getRegion();
+            double max = MatUtils.max(mat, region);
+            // Add to count if the nodule is completely black
+            if (max == 0) {
+              allBlackNodules.incrementAndGet();
+            }
+            HistogramWriter writer;
+            String dir;
+            if (max > DARK_LIGHT_THRESH) {
+              writer = lightWriter;
+              dir = LIGHT_DIR;
+            } else {
+              writer = darkWriter;
+              dir = DARK_DIR;
+            }
+
             // Write line to histogram file
             try {
-              writeLine(Histogram.createHist(gt.getRegion(), mat, BINS));
+              writer.writeLine(Histogram.createHist(region, mat, BINS));
             } catch (LungsException e) {
               LOGGER.error("Failed to create histogram for GroundTruth with id: " + gt.getId());
             }
 
-            // Annotate
+            // Create an annotated image and save it
             if (images) {
+              Mat bgr = grey2BGR(mat);
               for (Point point : gt.getEdgePoints()) {
-                bgr.put((int) point.y, (int) point.x, ColourBGR.RED);
+                put(bgr, point, ColourBGR.RED);
               }
+              Imgcodecs.imwrite(dir + "/" + sopUID + ".bmp", bgr);
             }
 
           }
 
-          // Save the mat
-          if (images) {
-            Imgcodecs.imwrite(IMAGE_DIR + "/" + sopUID + ".jpg", bgr);
-          }
-
-          // TODO remove this later
-        } catch (Exception e) {
-          LOGGER.error("Something went wrong when analysing slice with imageSopUID: " + sopUID, e);
-        }
-
-      }));
+        }));
 
       // Logging
       if (++counter % LOG_INTERVAL == 0) {
@@ -157,6 +185,37 @@ public class MissedNodules extends HistogramWriter {
     FutureMonitor monitor = new FutureMonitor(futures);
     monitor.setLogString("slices processed");
     monitor.monitor();
+
+    LOGGER.info("There were " + allBlackNodules.get() + " nodules that were completely black!");
+    
+    LOGGER.info("MissedNodules finished running");
+  }
+
+  private MultiMap<String, GroundTruth> buildUidToGt() {
+    // Get all the nodules that have been missed
+    Query<GroundTruth> query =
+        filter.singleReading(filter.all(ds.createQuery(GroundTruth.class).field("type")
+            .equal(BIG_NODULE).field("matchedToRoi").equal(false)));
+    long numMissed = query.count();
+    LOGGER.info(numMissed + " NODULES have been missed");
+
+    // Collect the nodules by imageSopUID
+    MultiMap<String, GroundTruth> uidToGt = new MultiMap<>();
+    query.cloneQuery().forEach(gt -> uidToGt.putOne(gt.getImageSopUID(), gt));
+    LOGGER.info(uidToGt.size() + " slices have missing nodules");
+
+    return uidToGt;
+  }
+
+  /**
+   * Delete the directories that images are stored in (if they exist), then create new ones.
+   */
+  private void resetDirs() {
+    File dir = new File(IMAGE_DIR);
+    dir.delete();
+    dir.mkdir();
+    new File(DARK_DIR).mkdir();
+    new File(LIGHT_DIR).mkdir();
   }
 
   public static void main(String[] args) throws LungsException {
