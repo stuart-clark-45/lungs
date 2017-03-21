0087e72909989013781a7fc585a1d28aa9f00a91
Stuart <stuart.clark_45@live.co.uk>
2017-03-21 21:47:18 +0000

    BlobDetector: gradient thresholding and added all params to constructor ready for optimisation

--- src/vision/BlobDetector.java
+++ src/vision/BlobDetector.java
@@ -1,11 +1,15 @@
 package vision;
 
+import static org.opencv.core.Core.BORDER_DEFAULT;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Size;
@@ -23,26 +27,32 @@ public class BlobDetector {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(BlobDetector.class);
 
+  private final static int SCALE = 1;
+  private final static int DELTA = 0;
+  private final static int DDEPTH = CvType.CV_16S;
+
   private final List<Integer> sigmaValues;
   private final int numSigmaValues;
   private final int xPadding;
   private final int yPadding;
-  private final int sigmaDiff;
+  private final int dogThresh;
+  private final int gradientThresh;
 
   public BlobDetector() {
-    this(new int[] {5, 5, 3});
+    this(new int[] {5, 5, 3}, 50, 5);
   }
 
   /**
    * @param neighbourhood The dimensions of the neighbourhood used for checking if a point in sigma
    *        space is a key point. Takes the form {width, height, sigmaSpace}
    */
-  public BlobDetector(int[] neighbourhood) {
-    xPadding = neighbourhood[1] / 2;
-    yPadding = neighbourhood[2] / 2;
-    sigmaValues = Arrays.asList(1, 3, 9, 15, 21, 27);
-    numSigmaValues = sigmaValues.size();
-    sigmaDiff = numSigmaValues / 2;
+  public BlobDetector(int[] neighbourhood, int dogThresh, int gradientThresh) {
+    this.xPadding = neighbourhood[1] / 2;
+    this.yPadding = neighbourhood[2] / 2;
+    this.sigmaValues = Arrays.asList(1, 3, 9, 15, 21, 27);
+    this.numSigmaValues = sigmaValues.size();
+    this.dogThresh = dogThresh;
+    this.gradientThresh = gradientThresh;
   }
 
   public List<KeyPoint> detect(Mat mat) {
@@ -73,13 +83,40 @@ public class BlobDetector {
       }
     }
 
-    return keyPoints;
+    // Gradient X
+    Mat gradX = new Mat(mat.rows(), mat.cols(), DDEPTH);
+    Imgproc.Sobel(mat, gradX, DDEPTH, 1, 1, 3, SCALE, DELTA, BORDER_DEFAULT);
+
+    // Gradient Y
+    Mat gradY = new Mat(mat.rows(), mat.cols(), DDEPTH);
+    Imgproc.Sobel(mat, gradY, DDEPTH, 0, 1, 3, SCALE, DELTA, BORDER_DEFAULT);
+
+    // Gradient X squared
+    Mat gradXPow2 = new Mat(mat.rows(), mat.cols(), DDEPTH);
+    Core.pow(gradX, 2, gradXPow2);
+
+    // Gradient Y squared
+    Mat gradYPow2 = new Mat(mat.rows(), mat.cols(), DDEPTH);
+    Core.pow(gradY, 2, gradYPow2);
+
+    // Calculate gradient magnitude
+    Mat sum = new Mat(mat.rows(), mat.cols(), CvType.CV_32FC1);
+    Core.add(gradXPow2, gradYPow2, sum);
+    Mat sumFloat = new Mat(mat.rows(), mat.cols(), CvType.CV_32FC1);
+    sum.convertTo(sumFloat, sumFloat.type());
+    Mat gradientMag = new Mat(mat.rows(), mat.cols(), CvType.CV_32FC1);
+    Core.sqrt(sumFloat, gradientMag);
+
+    // Filter out key points that are edges and return
+    return keyPoints.stream()
+        .filter(kp -> MatUtils.get(gradientMag, kp.getPoint())[0] < gradientThresh)
+        .collect(Collectors.toList());
   }
 
   private Optional<KeyPoint> createKeyPoint(int row, int col, int dogIndex, List<Mat> dogs) {
     Mat thisDog = dogs.get(dogIndex);
-     double thisVal = thisDog.get(row + yPadding, col + xPadding)[0];
-    if (thisVal < 50) {
+    double thisVal = thisDog.get(row + yPadding, col + xPadding)[0];
+    if (thisVal < dogThresh) {
       return Optional.empty();
     }
 
