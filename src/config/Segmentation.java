5535126c33c2678e99cc56e5c00ac1a1814faf7b
Stuart <stuart.clark_45@live.co.uk>
2017-03-21 21:50:36 +0000

    Segmentation: added Blob

--- src/config/Segmentation.java
+++ src/config/Segmentation.java
@@ -21,6 +21,16 @@ public class Segmentation {
     public static final String SIGMA_SPACE = "segmentation.filter.sigmaspace";
   }
 
+  public static class Blob {
+    private Blob() {}
+
+    public static final String NEIGHBOURHOOD_WIDTH = "segmentation.blob.neighbourhoodWidth";
+    public static final String NEIGHBOURHOOD_HEIGHT = "segmentation.blob.neighbourhoodHeight";
+    public static final String NEIGHBOURHOOD_DEPTH = "segmentation.blob.neighbourhoodDepth";
+    public static final String DOG_THRESH = "segmentation.blob.dogThresh";
+    public static final String GRADIENT_THRESH = "segmentation.blob.gradientThresh";
+  }
+
   public static final String SURE_FG = "segmentation.surefg";
   public static final String SURE_BG = "segmentation.surebg";
 
