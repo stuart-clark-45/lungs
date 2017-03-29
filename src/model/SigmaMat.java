b464bb2cace08919b5a5cd9846ce8a11d2cabd55
Stuart <stuart.clark_45@live.co.uk>
2017-03-29 15:48:11 +0100

    SigmaMat: scalar and javadoc

--- src/model/SigmaMat.java
+++ src/model/SigmaMat.java
@@ -1,6 +1,7 @@
 package model;
 
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
 
 /**
  * Simple model used to hold a {@link Mat} and it's corresponding sigma value.
@@ -9,12 +10,34 @@ import org.opencv.core.Mat;
  */
 public class SigmaMat {
 
+  /**
+   * The sigma value for the {@code mat}.
+   */
   private final double sigma;
+
   private final Mat mat;
+
+  /**
+   * The value used to scale co-ordinates to that they refer to the full size {@link Mat}.
+   */
+  private double scalar;
 
   public SigmaMat(Mat mat, double sigma) {
     this.mat = mat;
     this.sigma = sigma;
+    this.scalar = 1;
+  }
+
+  public Point getScaledPoint(int row, int col) {
+    return new Point(scalar * col, scalar * row);
+  }
+
+  public void setScalar(double scalar) {
+    this.scalar = scalar;
+  }
+
+  public double getScalar() {
+    return scalar;
   }
 
   public double getSigma() {
