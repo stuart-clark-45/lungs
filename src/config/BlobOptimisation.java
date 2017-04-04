13d12f40eab027b393937028d3a642a731a6710f
Stuart <stuart.clark_45@live.co.uk>
2017-04-04 16:14:49 +0100

    BlobOptimisation, application.conf: added key

---
+++ src/config/BlobOptimisation.java
@@ -0,0 +1,14 @@
+package config;
+
+/**
+ * The keys for configuration variables that relate to {@link optimise.BlobOpt}.
+ *
+ * @author Stuart Clark
+ */
+public class BlobOptimisation {
+
+  public static final String STACKS = "blobopt.stacks";
+
+  private BlobOptimisation() {}
+
+}
